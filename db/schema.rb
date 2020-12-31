df89536c945b95969c8a639dd0b511f4a4b965a1
mateomh <mmojicah.84@gmail.com>
2020-12-31 11:32:00 -0500

    Add name and username columns to the user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_31_161933) do
+ActiveRecord::Schema.define(version: 2020_12_31_162923) do
 
   create_table "tweeets", force: :cascade do |t|
     t.string "tweeet"
@@ -26,8 +26,11 @@ ActiveRecord::Schema.define(version: 2020_12_31_161933) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
