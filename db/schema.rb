047abfcc4bb33f2f66e6a1d939f71dae39200ff4
mateomh <mmojicah.84@gmail.com>
2020-12-31 12:31:34 -0500

    Add userid to the tweeets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_31_162923) do
+ActiveRecord::Schema.define(version: 2020_12_31_172950) do
 
   create_table "tweeets", force: :cascade do |t|
     t.string "tweeet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
